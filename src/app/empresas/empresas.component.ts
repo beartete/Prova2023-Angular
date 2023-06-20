b30a1e23fb27bfaaeca91d86621c88cb22f7901c
beartete <isabellacristinavieira2003@gmail.com>
2023-06-20 09:36:56 -0300

    Cadastrar

--- src/app/empresas/empresas.component.ts
+++ src/app/empresas/empresas.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registro-estudantes',
+  selector: 'app-empresas',
   templateUrl: './empresas.component.html',
   styleUrls: ['./empresas.component.css']
 })
